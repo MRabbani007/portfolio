35fd16c64ebdceb2c3774072006987eb6a3c1fad
Mohamad Rabbani <mrabbani@outlook.com>
2024-08-20 13:01:09 +0500

    bug fix

--- src/components/ui/MagicButton.tsx
+++ src/components/ui/MagicButton.tsx
@@ -16,7 +16,7 @@ export default function MagicButton({
 }) {
   // inset-[-1000%]
   return (
-    <div className="relative block overflow-hidden min-h-12 w-fit p-[1px] focus:outline-none my-4 mx-2">
+    <div className="relative block overflow-hidden min-h-12 w-fit p-[1px] focus:outline-none my-4 mx-2 z-0">
       <div className="absolute inset-[0] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#facc15_50%,#E2CBFF_100%)] z-0" />
       <div className="block h-full w-full cursor-pointer items-center justify-center bg-zinc-950/70 hover:bg-zinc-950/95 py-4 px-8 text-sm font-medium text-white backdrop-blur-3xl z-10">
         {position === "left" && icon}
